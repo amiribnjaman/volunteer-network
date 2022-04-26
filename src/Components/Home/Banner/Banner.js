dc1c5498cf1974236d132722dfb887762b0a8cbc
amiribnjaman <amiribnjaman@gmail.com>
2022-04-26 19:55:46 +0600

    Volunteer network porject navbar and banner img implemented

---
+++ src/Components/Home/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import BannerImg from '../../../Assets/Img/banner.png'
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner-img'>
+            
+        </div>
+    );
+};
+
+export default Banner;
